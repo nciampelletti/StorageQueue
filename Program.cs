41a850e4d514ff809d035272b1ac079170907e72
natasha <natashat@shaw.ca>
2023-08-12 16:28:10 -0700

    commit PickingMessage

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Windows.Markup;
+
+string connectionstring = "";
+string qname = "appqueue";
+
+//SendMessage("Test message 1");
+//SendMessage("Test message 2");
+
+PickMessage();
+
+void PickMessage()
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+	int maxMessage = 10;
+
+	PeekedMessage[] peekedMessages = queueClient.PeekMessages(maxMessage);
+
+	Console.WriteLine("The messages in the queue are: ");
+
+	foreach(var message in peekedMessages)
+	{
+		Console.WriteLine(message.Body);
+	}
+
+ }
+
+void SendMessage(string message)
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+
+	if (queueClient.Exists())
+	{
+		queueClient.SendMessage(message);
+		Console.WriteLine("Message has been sent");
+	}
+
+}
