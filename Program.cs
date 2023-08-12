e71555c1c03bcaa90ace0a164999c8cfdac74478
natasha <natashat@shaw.ca>
2023-08-12 16:33:07 -0700

    commit receiveMessages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ 
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Windows.Markup;
+
+string connectionstring = "DefaultEndpointsProtocol=https;AccountName=appstore4040ciamp;AccountKey=583ab8LTT1/BAogvgTGIWJftkBgkNRsrp6yJJ2D0alVnlv4d6IRGaTKuzs3fNNbNoEwcAgpg0SGC+AStGevc6g==;EndpointSuffix=core.windows.net";
+string qname = "appqueue";
+
+
+ReceiveMessage();
+
+
+void ReceiveMessage()
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+	int maxMessage = 10;
+
+	QueueMessage[] receivedMessages = queueClient.ReceiveMessages(maxMessage);
+
+	Console.WriteLine("The messages in the queue are: ");
+
+	foreach (var message in receivedMessages)
+	{
+		Console.WriteLine(message.Body);
+	}
+
+}
+
+void PickMessage()
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+	int maxMessage = 10;
+
+	PeekedMessage[] peekedMessages = queueClient.PeekMessages(maxMessage);
+
+	Console.WriteLine("The messages in the queue are: ");
+
+	foreach (var message in peekedMessages)
+	{
+		Console.WriteLine(message.Body);
+	}
+
+}
+
+void SendMessage(string message)
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+
+	if (queueClient.Exists())
+	{
+		queueClient.SendMessage(message);
+		Console.WriteLine("Message has been sent");
+	}
+
+}
