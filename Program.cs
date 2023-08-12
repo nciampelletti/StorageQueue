9cb2e888502cd2e19e26178b798f04ca63105882
natasha <natashat@shaw.ca>
2023-08-12 16:02:45 -0700

    commit SendingMessages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Storage.Queues;
+
+string connectionstring = "";
+string qname = "appqueue";
+
+SendMessage("Test message 1");
+SendMessage("Test message 2");
+
+void SendMessage(string message)
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+
+	if (queueClient.Exists())
+	{
+		queueClient.SendMessage(message);
+		Console.WriteLine("Message has been sent");
+	}
+
+}
