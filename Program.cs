8e6cef41fd5a539a570cd1ca72499911410adfac
natasha <natashat@shaw.ca>
2023-08-12 17:07:32 -0700

    commit SendingReceivingObjects

--- Program.cs
+++ Program.cs
@@ -1,19 +1,45 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
+using StorageQueue;
+using System.Net;
+using System.Text.Json.Serialization;
 
-string connectionstring = "";
+string connectionstring = "DefaultEndpointsProtocol=https;AccountName=appstore4040ciamp;AccountKey=583ab8LTT1/BAogvgTGIWJftkBgkNRsrp6yJJ2D0alVnlv4d6IRGaTKuzs3fNNbNoEwcAgpg0SGC+AStGevc6g==;EndpointSuffix=core.windows.net";
 string qname = "appqueue";
 
-SendMessage("Test message 1");
-SendMessage("Test message 2");
+SendMessage(3, "Natalia 2", "111 Bosa ave, Vancouver");
+SendMessage(4, "Emy 3", "22 Bosa ave, Vancouver");
 
-void SendMessage(string message)
+PickMessages();
+
+void SendMessage(int id, string name, string address)
 {
 	QueueClient queueClient = new QueueClient(connectionstring, qname);
 
 	if (queueClient.Exists())
 	{
-		queueClient.SendMessage(message);
+		Customer customer = new Customer { Id = id , Name = name, Address = address};
+		queueClient.SendMessage(JsonConvert.SerializeObject(customer));
 		Console.WriteLine("Message has been sent");
+	}
+
+}
+
+void PickMessages()
+{
+	QueueClient queueClient = new QueueClient(connectionstring, qname);
+	int maxMessage = 10;
+
+	PeekedMessage[] peekedMessages = queueClient.PeekMessages(maxMessage);
+
+	Console.WriteLine("The messages in the queue are: ");
+
+	foreach (var message in peekedMessages)
+	{
+		Customer customer =JsonConvert.DeserializeObject<Customer>(message.Body.ToString());
+
+		Console.WriteLine($"Customer Id: {customer.Id}");
 	}
 
 }
